12906219eaf932ecbfd395a70db08f9193af50d1
lunz <lunzjoao@gmail.com>
2024-08-03 00:55:11 -0300

    Scanners

---
+++ Testes/src/soma.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class soma {
+        public static void main(String[] args) {
+        System.err.println("Digite o primeiro número");
+        int numero1 = scanner.nextInt();
+        System.err.println("Digite o segundo número");
+        int numero2 = scanner.nextInt();
+
+        int soma = numero1 + numero2;
+        System.err.println("a soma destes dois números é:" + soma);
+    }
+}
